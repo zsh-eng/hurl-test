b122c5510ea4465d288bb187477e7fea2d1e58dd
sheng <chengzhisheng2001@gmail.com>
2023-07-26 09:36:41 +0800

    ci: add integration tests

---
+++ bin/integration.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu
+
+wait_for_url() {
+	echo "Testing $1..."
+	echo -e "GET $1\nHTTP 200" | hurl --retry "$2" >/dev/null
+	return 0
+}
+
+echo "Starting Quiz container"
+docker run --rm --detach --publish 8080:8080 --name quiz ghcr.io/jcamiel/quiz:latest
+
+echo "Starting Quiz instance to be ready"
+wait_for_url 'http://localhost:8080' 60
+
+echo "Running Hurl tests"
+hurl --test integration/*.hurl
+
+echo "Stopping Quiz instance"
+docker stop quiz
